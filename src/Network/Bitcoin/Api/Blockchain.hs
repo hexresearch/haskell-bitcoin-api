c15ba486c3dbf81fbf228dfa2e7a2bedee3d39de
Anton Gushcha <ncrashed@gmail.com>
2019-06-04 17:26:20 +0300

    Add gettransaction endpoint

--- src/Network/Bitcoin/Api/Blockchain.hs
+++ src/Network/Bitcoin/Api/Blockchain.hs
@@ -83,3 +83,13 @@ getRawTransaction ::
     -> IO (Maybe HS.HexString)  -- ^ Hex-encoded transaction
 getRawTransaction client txid =
   I.callMaybe client "getrawtransaction" [toJSON txid, toJSON (0 :: Integer)]
+
+-- | NOTE: Requires enabled transaction index in Bitcoin Core ("txindex" option)
+--      Fetch json-encoded transaction by transaction ID
+getTransaction ::
+    T.Client                    -- ^ Our session context
+    -> BT.TransactionId         -- ^ Transaction ID of transaction to fetch
+    -> Bool                     -- ^ Include whatch only addresses
+    -> IO (Maybe TXI.GetTxInfo)  -- ^ Transaction info
+getTransaction client txid whatchAddr =
+  I.callMaybe client "gettransaction" [toJSON txid, toJSON whatchAddr]
--- src/Network/Bitcoin/Api/Types/TxInfo.hs
+++ src/Network/Bitcoin/Api/Types/TxInfo.hs
@@ -12,7 +12,7 @@ import           Data.Aeson.Types
 
 import qualified Data.Bitcoin.Types  as BT
 
-
+import Data.Text (Text, unpack)
 
 data TxInfo = TxInfo {
    txid        :: BT.TransactionId
@@ -34,7 +34,78 @@ instance FromJSON TxInfo where
          <*> o .: "time"
   parseJSON _          = mzero
 
+data GetTxInfo = GetTxInfo {
+    getTxInfoAmount        :: !BT.Btc
+  , getTxInfoFee           :: !BT.Btc
+  , getTxInfoConfirmations :: !Integer
+  , getTxInfoBlockHash     :: !BT.BlockHash
+  , getTxInfoBlockIndex    :: !Int
+  , getTxInfoBlockTime     :: !Word64
+  , getTxInfoTxid          :: !BT.TransactionId
+  , getTxInfoTime          :: !Word64
+  , getTxInfoReceived      :: !Word64
+  , getTxInfoReplaceable   :: !Replacable
+  , getTxInfoHex           :: !Text
+  , getTxInfoDetails       :: ![TxInfoDetails]
+} deriving (Eq, Show)
 
+instance FromJSON GetTxInfo where
+  parseJSON (Object o) = GetTxInfo
+         <$> o .: "amount"
+         <*> o .: "fee"
+         <*> o .: "confirmations"
+         <*> o .: "blockhash"
+         <*> o .: "blockindex"
+         <*> o .: "blocktime"
+         <*> o .: "txid"
+         <*> o .: "time"
+         <*> o .: "timereceived"
+         <*> o .: "bip125-replaceable"
+         <*> o .: "hex"
+         <*> o .: "details"
+  parseJSON _          = mzero
+
+data TxInfoDetails = TxInfoDetails {
+    detailsAddress    :: !Text
+  , detailsCategory   :: !TxCategory
+  , detailsAmount     :: !BT.Btc
+  , detailsLabel      :: !Text
+  , detailsVout       :: !Int
+  , detailsFee        :: !BT.Btc
+  , detailsAbandoned  :: !Bool
+} deriving (Eq, Show)
+
+instance FromJSON TxInfoDetails where
+  parseJSON (Object o) = TxInfoDetails
+         <$> o .: "address"
+         <*> o .: "category"
+         <*> o .: "amount"
+         <*> o .: "label"
+         <*> o .: "vout"
+         <*> o .: "fee"
+         <*> o .: "abandoned"
+  parseJSON _          = mzero
+
+data TxCategory = TxSend | TxReceive
+  deriving (Eq, Show)
+
+instance FromJSON TxCategory where
+  parseJSON (String s) = case s of
+    "send" -> pure TxSend
+    "receive" -> pure TxReceive
+    v -> fail $ "Unknown value for TxInfoDetails " ++ unpack v
+  parseJSON _          = mzero
+
+data Replacable = RepleacableYes | ReplacableNot | ReplacableUnknown
+  deriving (Eq, Show)
+
+instance FromJSON Replacable where
+  parseJSON (String s) = case s of
+    "yes" -> pure RepleacableYes
+    "not" -> pure ReplacableNot
+    "unknown" -> pure ReplacableUnknown
+    v -> fail $ "Unknown value for Replacable " ++ unpack v
+  parseJSON _          = mzero
 
 data Vout = Vout {
    amount      :: BT.Btc
@@ -67,4 +138,3 @@ instance FromJSON Vin where
             Nothing -> return CoinbaseVin
             Just txid -> Vin txid <$> o .: "vout"
     parseJSON _          = mzero
-
